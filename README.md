72926b8c5276ed01a7eac9a02513a616818f40b9
Yaron Sheffer <yaronf@gmx.com>
2020-02-14 18:17:50 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,16 @@
+Probably the most dreary task when converting an old RFC into editable form is having to deal with the references.
+This simple script converts bibxml references extracted from xml2rfc files into markdown,
+for use in [kramdown-rfc2629](https://github.com/cabo/kramdown-rfc2629) Internet Drafts.
+It has been tested on a large number of references ([RFC 7525](https://tools.ietf.org/html/rfc7525#section-7)) but is likely to fail when faced with more complicated inputs. Contributions are welcome!
+
+The input is a **valid** XML file of the form:
+
+```XML
+<references>
+	<reference>
+	</reference>
+	...
+	<reference>
+	</reference>
+</references>
+```
---
+++ bibxml2md.py
@@ -0,0 +1,107 @@
+import xml.etree.ElementTree as ET 
+import yaml # pyyaml package
+import sys
+
+def removeNewline(str):
+    return str.replace('\n', ' ').replace('\r', '')
+
+def parseXML(infile):
+
+    tree = ET.parse(infile)
+
+    root = tree.getroot()
+    # Validate root is a "references" tag
+    if root.tag != 'references':
+        sys.exit("Root tag should be 'references'")
+
+    # Good to go, initialize output map
+    references = {}
+
+    for item in root.findall('./reference'):
+        anchor = item.get("anchor")
+        if anchor == None:
+            print("Reference has no anchor, skipping")
+            continue
+
+        # initialize output reference
+        reference = {}
+
+        target = item.get("target")
+        if target:
+            reference["target"] = target
+
+        title = removeNewline(item.find("front/title").text)
+        if title == "":
+            print("Warning: anchor '"+anchor+"' has no title")
+        else:
+            reference["title"] = title
+
+        # Collect authors
+        authors = []
+        for author in item.findall('front/author'):
+            yauthor = {}
+
+            fullname = author.get("fullname")
+            if fullname:
+                yauthor["name"] = fullname
+
+            initials = author.get("initials")
+            surname = author.get("surname")
+            if initials and surname:
+                ins = initials + " " + surname
+            elif initials:
+                ins = initials
+            elif surname:
+                ins = surname
+            else:
+                ins = None
+
+            if ins:
+                yauthor["ins"] = ins
+
+            org = author.find("organization")
+            if org is not None and org.text is not None:
+                yauthor["org"] = org.text
+
+            authors.append(yauthor)
+            
+        reference["author"] = authors
+
+        # The date
+        date = item.find("front/date")
+        if date is not None:
+            month = date.get("month")
+            year = date.get("year")
+            if month:
+                ydate = month + " " + year
+            elif year:
+                ydate = year
+            if month or year:
+                reference["date"] = ydate
+    
+        seriesInfoList = item.findall("seriesInfo")
+        if seriesInfoList != []:
+            yseriesInfo = {}
+            for si in seriesInfoList:
+                name = si.get("name")
+                value = si.get("value")
+                yseriesInfo[name] = value
+            reference["seriesInfo"] = yseriesInfo
+
+        references[anchor] = reference
+
+    # print(yaml.dump(references))
+    return references
+
+def main():
+    if len(sys.argv) != 3:
+        sys.exit("Usage: " + sys.argv[0] + " infile outfile")
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    refs = parseXML(infile)
+    out = open(outfile, "w")
+    out.write(yaml.dump(refs, width = float("inf"))) # work around pyyaml line break behavior
+
+if __name__ == "__main__":
+    main()
